27b3b4a76930d3f1c0f48cea6f37de02057cb54e
sundicide <kyusuen@gmail.com>
2022-09-13 20:48:42 +0900

    add function, buttons

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,22 @@ import 'package:flutter/material.dart';
 void main() => (runApp(MyApp()));
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answered!');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
       ),
-      body: Text('This is my default text!'),
+      body: Column(children: <Widget>[
+        Text('The question!'),
+        ElevatedButton(onPressed: answerQuestion,child: Text('Answer 1')),
+        ElevatedButton(onPressed: answerQuestion,child: Text('Answer 2')),
+        ElevatedButton(onPressed: answerQuestion,child: Text('Answer 3')),
+      ],),
     ),);
   }
 }
